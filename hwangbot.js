2ad2b8223c2ba88919e3f9d6193cfc1059381aa3
lhk <hayan7904@gmail.com>
2025-03-19 09:46:34 +0900

    get chat id

--- hwangbot.js
+++ hwangbot.js
@@ -8,8 +8,6 @@ const telegramBot = new TelegramBotApi(
 	{polling: true}
 );
 
-console.log(process.env.TELEGRAM_BOT_KEY);
-
 const sleep = function(ms) {
 	return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -28,6 +26,7 @@ const getDate = function(unixTime) {
 telegramBot.on('message', async (msg) => {
 
 	console.log(msg.from.id + ', ' + msg.from.first_name + ', ' + msg.from.last_name + ', ' + msg.from.username);
+	console.log(msg.chat.id);
 
 	// only msgs from our chat
 	if (msg.chat.id != 49819934) return;
