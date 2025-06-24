ffd0a5fc609b06bf35cc283be6183e691bf1cd7f
lhk <hayan7904@gmail.com>
2025-06-24 17:33:39 +0900

    variables 수정

--- bot/util/variables.js
+++ bot/util/variables.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    NO_BIRD_COUNT : process.env.NO_BIRD_COUNT || 15,
-    BLACKLISTS
+    NO_BIRD_COUNT: process.env.NO_BIRD_COUNT || 15,
+    BLACKLISTS: [52186264, 56796388]
 }
